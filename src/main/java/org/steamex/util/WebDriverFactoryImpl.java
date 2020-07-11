ec0632d16105c5084d868b25ab06cf9a98c5eb0a
Yves Mancera <yves.mancera@gmail.com>
2020-07-11 13:05:31 -0700

    add driver resources

---
+++ src/main/java/org/steamex/util/WebDriverFactoryImpl.java
@@ -0,0 +1,11 @@
+package org.steamex.util;
+
+import org.openqa.selenium.WebDriver;
+
+public class WebDriverFactoryImpl implements WebDriverFactory {
+
+    @Override
+    public WebDriver getDriver() {
+
+    }
+}
--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -2,4 +2,17 @@ import org.testng.annotations.*;
 
 public class SimpleTest {
 
+    public void setUp() {
+
+    }
+
+    @Test(groups = { "fast" })
+    public void aFastTest() {
+        System.out.println("Fast test");
+    }
+
+    @Test(groups = { "slow" })
+    public void aSlowTest() {
+        System.out.println("Slow test");
+    }
 }
